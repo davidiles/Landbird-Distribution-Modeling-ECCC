cfda920010c17c6da0106a10873aa7ddec8f877e
davidiles <36132264+davidiles@users.noreply.github.com>
2024-11-28 08:30:02 -0500

    Update 5_covariate_effects.R

--- Analysis/Ontario_BBA/Code/5_covariate_effects.R
+++ Analysis/Ontario_BBA/Code/5_covariate_effects.R
@@ -27,13 +27,7 @@ setwd(dirname)
 
 covariate_effect_table <- read.csv(paste0("../Output/Tables_Summaries/covariate_effect_table.csv"), header = TRUE)
 
-covariate_effect_table <- subset(covariate_effect_table,
-                                 species_name %!in% c("American Redstart","Black-and-white Warbler","Canada Jay","Chipping Sparrow","Downy Woodpecker","Eastern Phoebe","Northern Waterthrush","Tree Swallow","Swainson's Thrush"))
-
-covars_to_plot <- c("Hours_Since_Sunrise")
-covar_plots <- list()
-
-covar <- "Hours_Since_Sunrise"
+covar <- "PC3"
 tmp <- covariate_effect_table[which(covariate_effect_table[,covar] != 0),]
 colnum <- which(colnames(tmp) == covar)
 ggplot(data = tmp, aes(x = tmp[,colnum], y = pred_q50, ymin = pred_q05, ymax = pred_q95))+
@@ -45,6 +39,6 @@ ggplot(data = tmp, aes(x = tmp[,colnum], y = pred_q50, ymin = pred_q05, ymax = p
   ggtitle("Effect of Hours-Since-Sunrise")+
   theme_bw()+
   theme(legend.position = "none")+
-  facet_wrap(species_name~., scales = "free_y")+
+  facet_wrap(species_name~error_type, scales = "free_y")+
   geom_vline(xintercept = 0, linetype = 2)
 
